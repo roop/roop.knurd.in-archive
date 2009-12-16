1c6c1e10b35766efd2562f542d73b119d6bc9156
Roopesh Chander <roop@forwardbias.in>
2009-12-16 23:59:35 +0530

    use html explanations

--- js/explainKnurd.js
+++ js/explainKnurd.js
@@ -1,34 +1,14 @@
 function explainKnurd() {
-    var available_explanations = 3;
+    var availableExplanations = 10;
     var now = new Date();
-    var seconds = Math.floor(now.getTime() / 1000);
-    var whichth = seconds % available_explanations;
+    var minutes = Math.floor(now.getTime() / (1000 * 60));
+    var reexplainInterval = 2; // change every 2 minutes
+    var whichth = Math.floor(minutes / reexplainInterval) % availableExplanations;
     Ajax.Request('GET', '/js/explainKnurd/' + whichth, insertKnurdExplanation);
 }
 
 function insertKnurdExplanation(text) {
-    var explanation = text.split(" ");
-    var partOfSpeech = explanation.shift();
-    var partOfSpeechElem = document.createElement("b");
-    if (partOfSpeech.charAt(0) == '-') {
-        var italics = document.createElement("i");
-        italics.appendChild(document.createTextNode(partOfSpeech));
-        partOfSpeechElem.appendChild(italics);
-    } else {
-        explanation = text;
-    }
-    var explanationElem = document.createTextNode("  " + explanation);
-    var dictionaryEntry = document.createElement("span");
-    dictionaryEntry.id = "dictionaryentry";
-    dictionaryEntry.appendChild(partOfSpeechElem);
-    dictionaryEntry.appendChild(explanationElem);
-    var dictionaryKnurd = document.getElementById("dictionaryknurd");
-    var oldDictionaryEntry = document.getElementById("dictionaryentry");
-    if (oldDictionaryEntry != undefined) {
-        dictionaryKnurd.removeChild(oldDictionaryEntry);
-    }
-    var wordKnurd = document.getElementById("wordknurd");
-    dictionaryKnurd.appendChild(dictionaryEntry);
+    document.getElementById("dictionaryentry").innerHTML = text;
 }
 
 explainKnurd();
